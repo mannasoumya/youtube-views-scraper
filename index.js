7788097a260e837d86d37bf0a13e272e58ae0b55
Manna Soumya <sm323265@gmail.com>
2019-05-10 22:26:59 +0530

    Added Console Readline of YT URL and async/await
    Enter YT URL in the console and added async/await

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-const cheerio = require("cheerio");
 const fetch = require("node-fetch");
 const fs = require("fs");
-const yt_url = `your youtube url here`
-setInterval(getcount, 10000);
-function getcount() {
-  fetch(yt_url)
-    .then(res => res.text())
-    .then(body => {
-      const $ = cheerio.load(body.toString());
-      let bodystr = body.toString();
-      bodystr = bodystr.replace(/<script >/g, "<script>");
-      console.log($("div.watch-view-count").html());
-      let viewcount = $("div.watch-view-count").html().toString();
-      viewcount = viewcount.replace(/\D/g, "");
-      viewcount = viewcount + "," + new Date() + "\r\n";
-      fs.appendFileSync('log.txt', viewcount);
-    });
-}
+const cheerio = require("cheerio");
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+console.log("\033[2J");
+readline.question(`\r\nEnter YouTube Video URL :  `, yt_url => {
+  async function getcount() {
+    let res = await fetch(yt_url);
+    let body = await res.text();
+    let bodystr = body.toString();
+    bodystr = bodystr.replace(/<script >/g, "<script>");
+    const $ = cheerio.load(bodystr);
+    console.log($("div.watch-view-count").html());
+    let viewcount = $("div.watch-view-count")
+      .html()
+      .toString();
+    viewcount = viewcount.replace(/\D/g, "");
+    viewcount = viewcount + "," + new Date() + " @@" + "\r\n";
+    return viewcount;
+  }
+  readline.close();
+  setInterval(() => {
+    getcount()
+      .then(data => {
+        fs.appendFileSync("log.txt", data);
+      })
+      .catch(err => console.error(err));
+  }, 100);
+});
